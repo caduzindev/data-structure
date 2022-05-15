7cd351534743bcfbf39a1d1fe6ac3c564cfc3ca6
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-05-15 19:45:32 -0300

    feat: adicionado os metodos shift e unshift na LinkedList

--- single-linked-lists.js
+++ single-linked-lists.js
@@ -16,7 +16,6 @@ class SinglyLinkedList
     }
 
     push(val){
-        // na primeira execução seria o node1
         let node = new Node(val)
 
         if (!this.head) {
@@ -58,12 +57,48 @@ class SinglyLinkedList
         this.length--
         return removed
     }
+    shift(){
+        if (!this.head) return undefined
+        if (!this.head.next) {
+            let removed = this.head
+            this.head = null
+            this.tail = null
+            this.length = 0
+
+            return removed
+        }
+
+        let removed = this.head
+        this.head = this.head.next
+        this.length--
+
+        return removed
+
+    }
+    unshift(val){
+        let node = new Node(val)
+        if (!this.head) {
+            this.head = node
+            this.tail = this.head
+            this.length++
+
+            return node
+        }
+
+        node.next = this.head
+        this.head = node
+        this.length++
+        return node
+    }
 }
 
 list = new SinglyLinkedList()
-list.push('carlos')
-list.push('eduardo')
 
-console.log('total',list)
-console.log(list.pop())
-console.log('pop',list)
+console.log('original =>',list)
+list.unshift('carlos')
+list.unshift('Eduardo')
+list.unshift('Alemeida')
+console.log('atual =>',list)
+// console.log('total',list)
+// console.log(list.pop())
+// console.log('pop',list)
